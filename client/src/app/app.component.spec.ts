2e98523393cc2832d64d7866ca8f98afcf0a7112
joheiss <josef.heiss@jovisco.de>
2018-09-07 10:19:40 +0200

    client test

---
+++ client/src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  beforeEach(async(() => {
+  }));
+
+  beforeEach(() => {
+  });
+
+  it('should create the app', async(() => {
+  }));
+});
