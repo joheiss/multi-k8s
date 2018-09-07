0adbeba24bcccace4d484b50a122d61509c9953a
joheiss <josef.heiss@jovisco.de>
2018-09-07 10:36:37 +0200

    app test changed

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,17 +1,7 @@
-import {TestBed, async, ComponentFixture} from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterTestingModule} from '@angular/router/testing';
-import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {AppComponent} from './app.component';
+
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-  beforeEach(async(() => {
-  }));
 
-  beforeEach(() => {
+  it('should create the app', () => {
   });
-
-  it('should create the app', async(() => {
-  }));
 });
